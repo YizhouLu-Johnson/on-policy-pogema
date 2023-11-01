6e77e430e0be5ab93bcab920f4aa2ce11e85de86
YizhouLu_Johnson <yizhoulu51@gmail.com>
2023-11-01 11:02:29 -0700

    Pogema Bash

--- onpolicy/scripts/train_pogema_scripts/train_pogema.sh
+++ onpolicy/scripts/train_pogema_scripts/train_pogema.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+#SBATCH --account=skoenig_523
+#SBATCH --partition=main
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=32G
+#SBATCH --time=24:00:00
+module purge
+module load gcc/11.3.0
+module load conda
+conda activate marl
+
 env="Pogema"
 scenario="simple_speaker_listener"
 num_landmarks=3
@@ -13,6 +26,6 @@ do
     echo "seed is ${seed}:"
     CUDA_VISIBLE_DEVICES=0 python ../train/train_pogema.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
     --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} \
-    --n_training_threads 1 --n_rollout_threads 128 --num_mini_batch 1 --episode_length 25 --num_env_steps 2000000 \
+    --n_training_threads 1 --n_rollout_threads 16 --num_mini_batch 1 --episode_length 25 --num_env_steps 2000000 \
     --ppo_epoch 15 --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --wandb_name "xxx" --user_name "yuchao" --share_policy
 done
